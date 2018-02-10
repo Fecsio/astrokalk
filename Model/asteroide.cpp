396c47aa2d59b7c6269ce9eb80c12c06cb64642c
federico <federicocaldart@yahoo.it>
2018-02-10 16:45:01 +0100

    Aggiunti controlli bound velocita impatto

--- Model/asteroide.cpp
+++ Model/asteroide.cpp
@@ -20,7 +20,17 @@ double Asteroide::getVel() const{
 
 
 ConseguenzaCollisione Asteroide::Collisione() const{
-    long double ej = ((0.5)*Massa()*pow(10,12)*pow(velocita,2));
+    double vC = velocita;
+
+    if(vC< 11.2){
+        vC = 11.2;
+    }
+
+    else if(vC >72.8){
+        vC = 72.8;
+    }
+
+    long double ej = ((0.5)*Massa()*pow(10,12)*pow(vC,2));
     unsigned int eMT = (2.39e-16)*ej;
     double m = 0.67*log10(ej)-0.87;
     return ConseguenzaCollisione(eMT,m);
